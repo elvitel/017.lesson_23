1ca77445d7f8318f106a62dd993805818fcb5d06
Elena Tykhonova <sivak_elena@ukr.net>
2023-04-01 12:42:32 +0300

    home23 is added

---
+++ script.js
@@ -0,0 +1,24 @@
+ 
+
+
+// const divParent = document.getElementById('parent');
+// window.addEventListener('load', function() {
+//     const myTable = document.createElement('table');
+//     for (i = 0; i <= 9; i++) {
+//         const myRow = myTable.insertRow(i);        
+//         for (j = 0; j <= 9; j++) {
+//             const myCell = myRow.insertCell(j);  
+window.onload = function() {   
+    const divParent = document.getElementById('parent');                        
+    const myTable = document.createElement('table');
+    for (let i = 0; i <= 9; i++) {
+        const myRow = myTable.insertRow(i);        
+        for (let j = 0; j <= 9; j++) {
+            const myCell = myRow.insertCell(j);  
+            i === 0 ? myCell.innerHTML = j + 1 : myCell.innerHTML = i * 10 + j + 1; 
+            myCell.setAttribute('style', 'border: 1px solid black; width: 50px; height: 50px; text-align: center;');                               
+        }
+    }
+    myTable.style.border = '1px solid black';    
+    divParent.appendChild(myTable);       
+};    
